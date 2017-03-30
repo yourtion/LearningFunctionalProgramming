68fe99ab46c6d31dc8ce101980f6168294e1a883
YourtionGuo <yourtion@gmail.com>
2017-03-30 10:45:47 +0800

    Visitor Clojure 2

--- Clojure/src/com/yourtion/Pattern10/visitor_example.clj
+++ Clojure/src/com/yourtion/Pattern10/visitor_example.clj
@@ -31,16 +31,63 @@
          " "
          (-> this :address :street))))
 
+(defmulti test-multimethod (fn [keyword] keyword))
+
+(defmethod test-multimethod :foo [a-map]
+  "foo-method was called")
+
+(defmethod test-multimethod :bar [a-map]
+  "bar-method was called")
+
+(defmulti perimeter (fn [shape] (:shape-name shape)))
+(defmethod perimeter :circle [circle]
+  (* 2 Math/PI (:radius circle)))
+(defmethod perimeter :rectangle [rectangle]
+  (+ (* 2 (:width rectangle)) (* 2 (:height rectangle))))
+
+(def some-shapes [{:shape-name :circle :radius 4}
+                  {:shape-name :rectangle :width 2 :height 2}])
+
+(defmulti area (fn [shape] (:shape-name shape)))
+(defmethod area :circle [circle]
+  (* Math/PI (:radius circle) (:radius circle)))
+(defmethod area :rectangle [rectangle]
+  (* (:width rectangle) (:height rectangle)))
+
+(defmethod perimeter :square [square]
+  (* 4 (:side square)))
+(defmethod area :square [square]
+  (* (:side square) (:side square)))
+
+(def more-shapes (conj some-shapes
+                       {:shape-name :square :side 4}))
+
 (defn run []
   (println "SimplePerson : ")
   (def simple-person (->SimplePerson "Yourtion" "Guo" 123 "Fake. St."))
   (println (:first-name simple-person))
   (println (extract-name simple-person))
   (println (extract-address simple-person))
-
   (println "")
+
   (println "ComplexPerson : ")
   (def complex-person (->ComplexPerson {:first "yourtion" :last "guo"} {:house-num 456 :street "Fake2. St."}))
   (println (extract-name complex-person))
   (println (extract-address complex-person))
+  (println "")
+
+  (println "TestMultimethod : ")
+  (println (test-multimethod :foo))
+  (println (test-multimethod :bar))
+  (println "")
+
+  (println "SecondShapeExample :")
+  (println (for [shape some-shapes] (perimeter shape)))
+  (println (for [shape some-shapes] (area shape)))
+  (println "")
+
+  (println "ThirdShapeExample :")
+  (println (for [shape more-shapes] (perimeter shape)))
+  (println (for [shape more-shapes] (area shape)))
+
 )
