225c998c2eed92d2592d9ee5122d885edaac5203
YourtionGuo <yourtion@gmail.com>
2017-04-12 11:32:20 +0800

    All Scala demo at once

--- Scala/src/com/yourtion/PatternDemo/Example.scala
+++ Scala/src/com/yourtion/PatternDemo/Example.scala
@@ -1,16 +1,98 @@
 package com.yourtion.PatternDemo
 
-import com.yourtion.Pattern21.DSLExample
-
 /**
   * Created by Yourtion on 9/7/16.
   */
 object Example extends App {
 
   override def main(args: Array[String]): Unit = {
-    println("DSL Example : \n")
-    DSLExample.run()
-    println()
+
+    println("\nPattern01 Example : \n")
+    com.yourtion.Pattern01.PersonExample.run()
+    com.yourtion.Pattern01.PersonExampleEx.run()
+    println("------------------------------------")
+
+    println("\nPattern02 Example : \n")
+    com.yourtion.Pattern02.PersonExampleCC.run()
+    println("------------------------------------")
+
+    println("\nPattern03 Example : \n")
+    com.yourtion.Pattern03.RegisterClient.run()
+    println("------------------------------------")
+
+    println("\nPattern04 Example : \n")
+    com.yourtion.Pattern04.PersonHarness.run()
+    println("------------------------------------")
+
+    println("\nPattern05 Example : \n")
+    com.yourtion.Pattern05.HigherOrderFunctions.run()
+    com.yourtion.Pattern05.TheLambdaBarAndGrille.run()
+    println("------------------------------------")
+
+    println("\nPattern06 Example : \n")
+    com.yourtion.Pattern06.GradeReporter.run()
+    println("------------------------------------")
+
+    println("\nPattern07 Example : \n")
+    com.yourtion.Pattern07.PersonCollectorExample.run()
+    println("------------------------------------")
+
+    println("\nPattern08 Example : \n")
+    com.yourtion.Pattern08.PersonExamples.run()
+    println("------------------------------------")
+
+    println("\nPattern09 Example : \n")
+    com.yourtion.Pattern09.CalculatorExample.run()
+    println("------------------------------------")
+
+    println("\nPattern10 Example : \n")
+    com.yourtion.Pattern10.VisitorExample.run()
+    println("------------------------------------")
+
+    println("\nPattern11 Example : \n")
+    com.yourtion.Pattern11.Services.run()
+    println("------------------------------------")
+
+    println("\nPattern12 Example : \n")
+    com.yourtion.Pattern12.Names.run()
+    println("------------------------------------")
+
+    println("\nPattern13 Example : \n")
+    com.yourtion.Pattern13.MutualRecursionExample.run()
+    println("------------------------------------")
+
+    println("\nPattern14 Example : \n")
+    com.yourtion.Pattern14.DiscountExample.run()
+    println("------------------------------------")
+
+    println("\nPattern15 Example : \n")
+    com.yourtion.Pattern15.ChainExample.run()
+    println("------------------------------------")
+
+    println("\nPattern16 Example : \n")
+    com.yourtion.Pattern16.FunctionBuilderExample.run()
+    println("------------------------------------")
+
+    println("\nPattern17 Example : \n")
+    com.yourtion.Pattern17.MemoizationExample.run()
+    println("------------------------------------")
+
+    println("\nPattern18 Example : \n")
+    com.yourtion.Pattern18.LazySequenceExample.run()
+    println("------------------------------------")
+
+    println("\nPattern19 Example : \n")
+    com.yourtion.Pattern19.FocusedMutationExample.run()
+    println("------------------------------------")
+
+    println("\nPattern20 Example : \n")
+    com.yourtion.Pattern20.FlowControlExample.run()
+    println("------------------------------------")
+
+    println("\nPattern21 Example : \n")
+    com.yourtion.Pattern21.DSLExample.run()
+    println("------------------------------------")
+
   }
 
 }
