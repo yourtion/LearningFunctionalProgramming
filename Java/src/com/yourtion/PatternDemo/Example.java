1c94658e438e5aa291504eb743400f8158715967
YourtionGuo <yourtion@gmail.com>
2017-04-12 11:32:07 +0800

    All Java demo at once

--- Java/src/com/yourtion/PatternDemo/Example.java
+++ Java/src/com/yourtion/PatternDemo/Example.java
@@ -1,18 +1,55 @@
 package com.yourtion.PatternDemo;
 
-import com.yourtion.Pattern15.ChainExample;
-
 /**
  * Created by Yourtion on 9/7/16.
  */
 public class Example {
 
     public static void main(String[] args){
-        System.out.println("Chain Example : \n");
+        System.out.println("\nPattern01 Example : \n");
+        new com.yourtion.Pattern01.PersonExample().run();
+        System.out.println("------------------------------------");
 
-        new ChainExample().run();
+        System.out.println("\nPattern02 Example : \n");
+        new com.yourtion.Pattern02.PersonExampleCC().run();
+        System.out.println("------------------------------------");
 
-        System.out.println("");
+        System.out.println("\nPattern03 Example : \n");
+        new com.yourtion.Pattern03.RegisterClient().run();
+        System.out.println("------------------------------------");
+
+        System.out.println("\nPattern04 Example : \n");
+        new com.yourtion.Pattern04.PersonHarness().run();
+        System.out.println("------------------------------------");
+
+        System.out.println("\nPattern05 Example : \n");
+        new com.yourtion.Pattern05.HigherOrderFunctions().run();
+        new com.yourtion.Pattern05.TheLambdaBarAndGrille().run();
+        System.out.println("------------------------------------");
+
+        System.out.println("\nPattern06 Example : \n");
+        new com.yourtion.Pattern06.FullGradeReporter().run();
+        System.out.println("------------------------------------");
+
+        System.out.println("\nPattern07 Example : \n");
+        new com.yourtion.Pattern07.PersonCollectorExample().run();
+        System.out.println("------------------------------------");
+
+        System.out.println("\nPattern08 Example : \n");
+        new com.yourtion.Pattern08.PersonExample().run();
+        System.out.println("------------------------------------");
+
+        System.out.println("\nPattern09 Example : \n");
+        new com.yourtion.Pattern09.CalculatorExample().run();
+        System.out.println("------------------------------------");
+
+        System.out.println("\nPattern12 Example : \n");
+        new com.yourtion.Pattern12.SumExample().run();
+        System.out.println("------------------------------------");
+
+        System.out.println("\nPattern15 Example : \n");
+        new com.yourtion.Pattern15.ChainExample().run();
+        System.out.println("------------------------------------");
 
     }
 }
